4c6a5a935dedb1872c9b16d9ecc43791b75a848c
ZHZisZZ <asap.zzhou@gmail.com>
2025-10-03 13:23:52 +0800

    editflow: add llada modeling and minor fix dream modeling

--- dllm/pipelines/editflow/models/llada/modelling_llada.py
+++ dllm/pipelines/editflow/models/llada/modelling_llada.py
@@ -4,21 +4,21 @@ from typing import Optional
 import torch 
 from torch import nn
 
-from dllm.pipelines import dream
+from dllm.pipelines import llada
 
 
-class EditFlowDreamConfig(dream.DreamConfig):
-    model_type = "editflow-dream"  # <- NEW model_type
+class EditFlowLLaDAConfig(llada.LLaDAConfig):
+    model_type = "editflow-llada"  # <- NEW model_type
 
 
-class EditFlowDreamModel(dream.DreamModel):
-    config_class = EditFlowDreamConfig
+class EditFlowLLaDAModel(llada.LLaDAModelLM):
+    config_class = EditFlowLLaDAConfig
 
     def __init__(self, config):
         # TODO: time embedding
         super().__init__(config)
-        self.sub_logits = copy.deepcopy(self.lm_head)
-        self.ins_logits = copy.deepcopy(self.lm_head)
+        self.sub_logits = copy.deepcopy(self.model.transformer.ff_out)
+        self.ins_logits = copy.deepcopy(self.model.transformer.ff_out)
         self.rate_heads = nn.Sequential(nn.Linear(config.hidden_size, 3), nn.Softplus())
         self.post_init()
 
@@ -39,13 +39,10 @@ class EditFlowDreamModel(dream.DreamModel):
         h = output["hidden_states"][-1] # final hidden states
         # Position heads
         sub_log = self.sub_logits(h)      # [B, L, V]
-        sub_log = torch.concatenate([torch.zeros_like(sub_log)[:, :1], sub_log[:, :-1]], dim=1) # [B, L, V]
         ins_log = self.ins_logits(h)      # [B, L, V]
 
         rates = self.rate_heads(h)
         sub_rate_hat, del_rate_hat, ins_rate_hat = rates.unbind(-1)    # [B, L], [B, L], [B, L]
-        sub_rate_hat = torch.concatenate([torch.zeros_like(sub_rate_hat[:, :1]), sub_rate_hat[:, :-1]], dim=1) # [B, L]
-        del_rate_hat = torch.concatenate([torch.zeros_like(del_rate_hat[:, :1]), del_rate_hat[:, :-1]], dim=1) # [B, L]
         return dict(
             sub_rate_hat=sub_rate_hat,    # [B,L]
             del_rate_hat=del_rate_hat,    # [B,L]
@@ -57,8 +54,8 @@ class EditFlowDreamModel(dream.DreamModel):
 
 from transformers.models.auto import AutoModel, AutoConfig
 # Register the model so that it is available for transformer pipelines, auto-loading, etc.
-AutoConfig.register("editflow-dream", EditFlowDreamConfig)
-AutoModel.register(EditFlowDreamConfig, EditFlowDreamModel)
+AutoConfig.register("editflow-llada", EditFlowLLaDAConfig)
+AutoModel.register(EditFlowLLaDAConfig, EditFlowLLaDAModel)
 
 
 if __name__ == "__main__":
@@ -68,14 +65,14 @@ if __name__ == "__main__":
 
     # Load a config from a local path (either a directory containing config.json, or the file itself)
     config_path = dllm.utils.resolve_with_base_env(
-        "Dream-org/Dream-v0-Base-7B", "BASE_MODELS_DIR")
-    config = EditFlowDreamConfig.from_pretrained(config_path)
+        "GSAI-ML/LLaDA-8B-Base", "BASE_MODELS_DIR")
+    config = EditFlowLLaDAConfig.from_pretrained(config_path)
     if hasattr(config, "auto_map"):
         delattr(config, "auto_map")
     if hasattr(config, "architectures"):
         delattr(config, "architectures")
 
     torch.set_default_device("cuda")
-    model = EditFlowDreamModel(config)
-    model.save_pretrained("models-tmp/editflow-dream")
-    auto_model = AutoModel.from_pretrained("models-tmp/editflow-dream")
+    model = EditFlowLLaDAModel(config)
+    model.save_pretrained("models-tmp/editflow-llada")
+    auto_model = AutoModel.from_pretrained("models-tmp/editflow-llada")
