ae0fcf9e667d1aa853a2baa9da49dc638c52b0ae
ZHZisZZ <asap.zzhou@gmail.com>
2025-10-24 07:52:48 +0800

    pipeline generate -> generation

--- dllm/pipelines/dream/__init__.py
+++ dllm/pipelines/dream/__init__.py
@@ -1,6 +1,6 @@
-from . import models, generate, trainer, utils
+from . import generation, models, trainer, utils
 from .models.modeling_dream import DreamModel
 from .models.configuration_dream import DreamConfig
 from .models.tokenization_dream import DreamTokenizer
-from .generate import generate, infilling
+from .generation import generate, infilling
 from .trainer import DreamTrainer
--- dllm/pipelines/llada/__init__.py
+++ dllm/pipelines/llada/__init__.py
@@ -1,7 +1,7 @@
-from . import generate, trainer
+from . import generation, trainer
 from .models.modeling_llada import LLaDAModelLM
 from .models.configuration_llada import LLaDAConfig
 from .models.modeling_lladamoe import LLaDAMoEModelLM
 from .models.configuration_lladamoe import LLaDAMoEConfig
-from .generate import generate, infilling
+from .generation import generate, infilling
 from .trainer import LLaDATrainer
